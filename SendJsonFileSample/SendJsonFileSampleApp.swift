0362f8c05c5717fc548bd701e5f7b2b379f4e1e7
三浦一真 <miurakazuma67@gmail.com>
2024-05-19 17:38:58 +0900

    Initial Commit

---
+++ SendJsonFileSample/SendJsonFileSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendJsonFileSampleApp.swift
+//  SendJsonFileSample
+//
+//  Created by 三浦一真 on 2024/05/19.
+//
+
+import SwiftUI
+
+@main
+struct SendJsonFileSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
